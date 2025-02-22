b12a988cda9ce023c962e321f1d84709d04dc29a
julio zimmermann <julioczn@gmail.com>
2025-02-22 20:56:32 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Description
+
+A simple transaction authorizer service.
+
+## Project setup
+
+```bash
+$ yarn install
+```
+
+## Compile and run the project
+
+```bash
+# development
+$ yarn run start
+
+# watch mode
+$ yarn run start:dev
+
+# production mode
+$ yarn run start:prod
+```
+
+## Run tests
+
+```bash
+# unit tests
+$ yarn run test
+
+# e2e tests
+$ yarn run test:e2e
+
+# test coverage
+$ yarn run test:cov
+```
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { patchNestjsSwagger, ZodValidationPipe } from '@anatine/zod-nestjs';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+	app.use(helmet());
+
+	app.enableCors();
+
+	app.useBodyParser('json', { limit: '1mb' });
+	app.useBodyParser('urlencoded', { extended: true, limit: '1mb' });
+
+	app.useGlobalPipes(new ZodValidationPipe());
+
+	patchNestjsSwagger();
+
+	const config = new DocumentBuilder()
+		.setTitle('Transaction Authorizer API')
+		.setVersion('1.0')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
+	await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();
